0e0fb9610e7cd4d26e45bdfa3a69b3ab835f9b33
Olyn <carolyn.ardjo@gmail.com>
2025-10-23 00:03:44 +0700

    update : add number of items pop up on cart and fix padding

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,48 +1,67 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 import { BsCart } from "react-icons/bs";
+import { useCart } from "../context/CartContext";
 
 function NavBar() {
+  const { cart } = useCart();
+  const itemCount = cart.reduce((sum, item) => sum + item.quantity, 0);
+
   return (
     <Navbar expand="lg" bg="white" data-bs-theme="light" className="sticky-top">
-      <Container className="py-1">
-        <Navbar.Brand
-          href="#home"
-          style={{
-            letterSpacing: "2px",
-            fontWeight: 800,
-          }}
-        >
-          FRUITOPIA
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav
-            className="ms-auto"
+      <Container className="py-1 px-md-2">
+        {/* LEFT SIDE - BRAND */}
+        <div className="d-flex align-items-center">
+          <Navbar.Brand
+            as={Link}
+            to="/"
             style={{
-              fontWeight: 600,
+              letterSpacing: "2px",
+              fontWeight: 800,
             }}
           >
+            FRUITOPIA
+          </Navbar.Brand>
+        </div>
+
+        {/* RIGHT SIDE (CART + TOGGLER) */}
+        <div className="d-flex align-items-center">
+          {/* CART ICON (small screens only) */}
+          <div className="d-lg-none position-relative me-2">
+            <Link to="/cart" className="text-dark position-relative">
+              <BsCart size={22} />
+              {itemCount > 0 && (
+                <span
+                  className="position-absolute badge rounded-pill bg-danger text-white"
+                  style={{
+                    top: "-8px",
+                    right: "-10px",
+                    fontSize: "0.6rem",
+                    minWidth: "18px",
+                    height: "18px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  {itemCount}
+                </span>
+              )}
+            </Link>
+          </div>
+
+          {/* TOGGLER */}
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        </div>
+
+        {/* COLLAPSIBLE MENU */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto fw-semibold align-items-lg-center">
             <Nav.Link as={Link} to="/" className="mx-2">
               Home
             </Nav.Link>
-            {/* <Nav.Link as={Link} to="/about" className="mx-2">
-              About
-            </Nav.Link> */}
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
             <Nav.Link as={Link} to="/shop" className="mx-2">
               Shop
             </Nav.Link>
@@ -52,8 +71,33 @@ function NavBar() {
             <Nav.Link as={Link} to="/contact" className="mx-2">
               Contact
             </Nav.Link>
-            <Nav.Link as={Link} to="/cart" className="mx-2">
-              <BsCart size={20} style={{ marginTop: "-3px" }} />
+
+            {/* CART ICON (large screens only) */}
+            <Nav.Link
+              as={Link}
+              to="/cart"
+              className="mx-2 position-relative d-none d-lg-block"
+            >
+              <div style={{ position: "relative", display: "inline-block" }}>
+                <BsCart size={22} style={{ marginTop: "-3px" }} />
+                {itemCount > 0 && (
+                  <span
+                    className="position-absolute badge rounded-pill bg-danger text-white"
+                    style={{
+                      top: "-8px",
+                      right: "-10px",
+                      fontSize: "0.6rem",
+                      minWidth: "18px",
+                      height: "18px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {itemCount}
+                  </span>
+                )}
+              </div>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
